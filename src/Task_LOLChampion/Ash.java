320afe073d241d0128ab2c62d3f5c2db1d14a3b5
MinWoo1995 <wjsalsdn120@naver.com>
2025-12-09 19:30:00 +0900

    Initial project setup

---
+++ src/Task_LOLChampion/Ash.java
@@ -0,0 +1,19 @@
+package Task_LOLChampion;
+
+public class Ash {
+    String name;
+    int levle;
+    int attackDamage;
+    int defenseDamage;
+    int HP;
+    int MP;
+
+    public Ash(String name,int level,int attackDamage,int defenseDamage,int HP,int MP) {
+        this.name = name;
+        this.levle = level;
+        this.attackDamage=attackDamage;
+        this.defenseDamage = defenseDamage;
+        this.HP = HP;
+        this.MP = MP;
+    }
+}
---
+++ src/Task_LOLChampion/Garen.java
@@ -0,0 +1,20 @@
+package Task_LOLChampion;
+
+public class Garen {
+    String name;
+    int levle;
+    int attackDamage;
+    int defenseDamage;
+    int HP;
+    int MP;
+
+    public Garen(String name,int level,int attackDamage,int defenseDamage,int HP,int MP) {
+        this.name = name;
+        this.levle = level;
+        this.attackDamage=attackDamage;
+        this.defenseDamage = defenseDamage;
+        this.HP = HP;
+        this.MP = MP;
+    }
+
+}
